955339d034ab8eec10f482e7a26e08b0276c212a
Crazy-pro <Crazy.pro.noob@gmail.com>
2024-01-11 18:17:59 +0100

    add type guards

--- section-06-project-01-advanced-types/src/app.ts
+++ section-06-project-01-advanced-types/src/app.ts
@@ -23,3 +23,59 @@ type Combinable = string | number
 type Numeric = number | boolean
 
 type Universal = Combinable & Numeric
+
+const add = (a: Combinable, b: Combinable) => {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString()
+  }
+  return a + b
+}
+
+type UnknownEmployee = Employee | Admin
+
+const printEmployeeInfo = (employee: UnknownEmployee) => {
+  console.log(`Name: ${employee.name}`)
+  if ('privileges' in employee) {
+    console.log(`Privileges: ${employee.privileges}`)
+  }
+  if ('startDate' in employee) {
+    console.log(`Start Date: ${employee.startDate}`)
+  }
+}
+
+printEmployeeInfo({
+  name: 'Alex',
+  startDate: new Date()
+})
+
+class Car {
+  drive() {
+    console.log('Driving a car...')
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck...')
+  }
+
+  loadCargo(amount: number) {
+    console.log(`Loading cargo... ${amount}`)
+  }
+}
+
+type Vehicle = Car | Truck
+
+const v1 = new Car()
+
+const v2 = new Truck()
+
+const useVehicle = (vehicle: Vehicle) => {
+  vehicle.drive()
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(777)
+  }
+}
+
+useVehicle(v1)
+useVehicle(v2)
