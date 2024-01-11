8e75714235690588e9ca46053a408748e8b5860b
Crazy-pro <Crazy.pro.noob@gmail.com>
2024-01-11 18:29:12 +0100

    add type casting

--- section-06-project-01-advanced-types/src/app.ts
+++ section-06-project-01-advanced-types/src/app.ts
@@ -110,4 +110,12 @@ moveAnimal({
   flyingSpeed: 49
 })
 
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!
 
+// const userInputElement = document.getElementById('user-input')! as HTMLInputElement
+
+const userInputElement = document.getElementById('user-input')
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = 'Hi there'
+}
