96f306e21c422bc1c73ec9cf63bcce2fe441f832
Crazy-pro <Crazy.pro.noob@gmail.com>
2024-01-11 18:36:47 +0100

    add functions overloading

--- section-06-project-01-advanced-types/src/app.ts
+++ section-06-project-01-advanced-types/src/app.ts
@@ -24,12 +24,19 @@ type Numeric = number | boolean
 
 type Universal = Combinable & Numeric
 
-const add = (a: Combinable, b: Combinable) => {
+function add(a: number, b: number): number
+function add(a: string, b: string): string
+function add(a: string, b: number): string
+function add(a: number, b: string): string
+function add(a: Combinable, b: Combinable) {
   if (typeof a === 'string' || typeof b === 'string') {
     return a.toString() + b.toString()
   }
   return a + b
 }
+
+const result = add('Alex', ' Klimchuk')
+result.split(' ')
 
 type UnknownEmployee = Employee | Admin
 
@@ -118,4 +125,14 @@ const userInputElement = document.getElementById('user-input')
 
 if (userInputElement) {
   (userInputElement as HTMLInputElement).value = 'Hi there'
+}
+
+interface ErrorContainer { // { email: 'Not a valid Email!', userName: 'Must start with a capital character!' }
+  // id: string // Cannot be number!
+  [prop: string]: string
+}
+
+const errorBag: ErrorContainer = {
+  email: 'Not a valid Email!',
+  userName: 'Must start with a capital character!'
 }
